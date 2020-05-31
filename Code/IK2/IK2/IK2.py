3cd4f34371450f124cf116776957e502c007fd00
Luka Bouchard <1732142@cegeplimoilou.ca>
2020-05-30 21:05:38 -0400

    modified IK

--- Code/IK2/IK2/IK2.py
+++ Code/IK2/IK2/IK2.py
@@ -9,17 +9,17 @@ from mpl_toolkits.mplot3d import Axes3D
 ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
 
 #Open URDF file of arm for links and chains
-my_chain = ikpy.chain.Chain.from_urdf_file("C:/Users/lukab/Desktop/6DOF/Code/IK2/IK2/ur3_robot.urdf")
+my_chain = ikpy.chain.Chain.from_urdf_file("ur3_robot.urdf")
 
 #cartisean coordinates in m (input in cm)
 x = float(input("X:")) / 100
 y = float(input("Y:")) / 100
 z = float(input("Z:")) / 100
 
-#Martice for end-effector's position
+#Matrice for end-effector's position
 target_position = [x, y, z]
 
-#list of joints angles
+#List of joints angles
 angles = my_chain.inverse_kinematics(target_position)
 
 #Convert radians to degrees
@@ -29,11 +29,11 @@ for a in range(len(angles) - 2):
 #Add offset to angles (homing 0's)
 angles[1] = angles[1] + 90
 angles[2] = angles[2] + 135
-angles[2] = angles[3] + 110
+angles[3] = angles[3] + 110
 #angles[2] = angles[4] +
 #angles[2] = angles[5] +
 
-#To print joints angles
+#Print joints angles
 i = 0
 for a in range(len(angles) - 2):
     i += 1
